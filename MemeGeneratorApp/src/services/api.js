e43b21e93711648e3d513d3ba6dd1cd118a181bf
GABRIEL <keubengg@gmail.com>
2026-06-29 10:04:11 +0200

    Backend déployé sur Render

--- MemeGeneratorApp/src/services/api.js
+++ MemeGeneratorApp/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  timeout: 30000,
+  baseURL: 'https://memegenerator-backend-1img.onrender.com/api',
+  timeout: 60000,
 });
 
 export default api;
